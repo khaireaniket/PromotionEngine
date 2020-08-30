80c37a90f3d177fb30d42eba218cad2379e8ad90
khaireaniket <khaireaniket@gmail.com>
2020-08-30 19:59:09 +0530

    Created a mock database to replicate promotion and sku collection

---
+++ PromotionEngine/Data/MockDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using PromotionEngine.Models.DBModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PromotionEngine.Data
+{
+    class MockDatabase
+    {
+        public static List<PromotionCollection> PromotionCollection { get; set; }
+        public static List<SkuCollection> SkuCollection { get; set; }
+
+        public static void Seed()
+        {
+            PromotionCollection = JsonConvert.DeserializeObject<List<PromotionCollection>>(File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/MockData/MockPromotions.json"));
+            SkuCollection = JsonConvert.DeserializeObject<List<SkuCollection>>(File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/MockData/MockSku.json"));
+        }
+    }
+}
--- PromotionEngine/Service/Engine.cs
+++ PromotionEngine/Service/Engine.cs
@@ -1,7 +1,22 @@
-﻿namespace PromotionEngine.Service
+﻿using PromotionEngine.Data;
+using PromotionEngine.Models.DBModel;
+using System.Collections.Generic;
+
+namespace PromotionEngine.Service
 {
     public class Engine
     {
+        List<PromotionCollection> PromotionCollection;
+        List<SkuCollection> SkuCollection;
 
+        public Engine()
+        {
+            // Seed mock database
+            MockDatabase.Seed();
+
+            // Get promotion and Sku collections from mock database
+            PromotionCollection = MockDatabase.PromotionCollection;
+            SkuCollection = MockDatabase.SkuCollection;
+        }
     }
 }
