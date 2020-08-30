8aa45a93a50cf2b31e2fad95935effb9e3d92740
khaireaniket <khaireaniket@gmail.com>
2020-08-30 19:53:32 +0530

    Modified unit test with expected input and output

--- PromotionEngine.Tests/Service/EngineTest.cs
+++ PromotionEngine.Tests/Service/EngineTest.cs
@@ -1,3 +1,5 @@
+using PromotionEngine.Models.DomainModel;
+using PromotionEngine.Service;
 using Xunit;
 
 namespace PromotionEngine.Tests.Service
@@ -7,7 +9,16 @@ namespace PromotionEngine.Tests.Service
         [Fact]
         public void Test_PromotionEngine()
         {
+            // Arrange
+            Cart cart = new Cart();
+            Engine engine = new Engine();
+            decimal expectedCartTotal = 0;
 
+            // Act
+            engine.Run(cart);
+
+            // Assert
+            Assert.Equal(expectedCartTotal, cart.CartTotal);
         }
     }
 }
