9f2e730e7f19ac545c39772087df83fc70ba6dce
khaireaniket <khaireaniket@gmail.com>
2020-08-30 20:11:36 +0530

    Passing actual input (scenario 1) and output (100) to the unit test, which is expected to fail

--- PromotionEngine.Tests/Service/EngineTest.cs
+++ PromotionEngine.Tests/Service/EngineTest.cs
@@ -1,5 +1,6 @@
 using PromotionEngine.Models.DomainModel;
 using PromotionEngine.Service;
+using System.Collections.Generic;
 using Xunit;
 
 namespace PromotionEngine.Tests.Service
@@ -10,9 +11,18 @@ namespace PromotionEngine.Tests.Service
         public void Test_PromotionEngine()
         {
             // Arrange
-            Cart cart = new Cart();
+            Cart cart = new Cart
+            {
+                PurchasedSkus = new List<PurchasedSku>
+                                    {
+                                        new PurchasedSku { SkuId = "A", Quantity = 1 },
+                                        new PurchasedSku { SkuId = "B", Quantity = 1 },
+                                        new PurchasedSku { SkuId = "C", Quantity = 1 }
+                                    }
+            };
+
             Engine engine = new Engine();
-            decimal expectedCartTotal = 0;
+            decimal expectedCartTotal = 100;
 
             // Act
             engine.Run(cart);
