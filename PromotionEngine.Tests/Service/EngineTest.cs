feaa3d6f64873ef78b0677159cf8f0aa31159147
khaireaniket <khaireaniket@gmail.com>
2020-08-30 20:46:20 +0530

    Added more scenarios to the unit test, which are expected to fail

--- PromotionEngine.Tests/Service/EngineTest.cs
+++ PromotionEngine.Tests/Service/EngineTest.cs
@@ -8,7 +8,7 @@ namespace PromotionEngine.Tests.Service
     public class EngineTest
     {
         [Fact]
-        public void Test_PromotionEngine()
+        public void Test_PromotionEngine_Scenario_1()
         {
             // Arrange
             Cart cart = new Cart
@@ -23,6 +23,55 @@ namespace PromotionEngine.Tests.Service
 
             Engine engine = new Engine();
             decimal expectedCartTotal = 100;
+
+            // Act
+            engine.Run(cart);
+
+            // Assert
+            Assert.Equal(expectedCartTotal, cart.CartTotal);
+        }
+
+        [Fact]
+        public void Test_PromotionEngine_Scenario_2()
+        {
+            // Arrange
+            Cart cart = new Cart
+            {
+                PurchasedSkus = new List<PurchasedSku>
+                                    {
+                                        new PurchasedSku { SkuId = "A", Quantity = 5 },
+                                        new PurchasedSku { SkuId = "B", Quantity = 5 },
+                                        new PurchasedSku { SkuId = "C", Quantity = 1 }
+                                    }
+            };
+
+            Engine engine = new Engine();
+            decimal expectedCartTotal = 370;
+
+            // Act
+            engine.Run(cart);
+
+            // Assert
+            Assert.Equal(expectedCartTotal, cart.CartTotal);
+        }
+
+        [Fact]
+        public void Test_PromotionEngine_Scenario_3()
+        {
+            // Arrange
+            Cart cart = new Cart
+            {
+                PurchasedSkus = new List<PurchasedSku>
+                                    {
+                                        new PurchasedSku { SkuId = "A", Quantity = 3 },
+                                        new PurchasedSku { SkuId = "B", Quantity = 5 },
+                                        new PurchasedSku { SkuId = "C", Quantity = 1 },
+                                        new PurchasedSku { SkuId = "D", Quantity = 1 }
+                                    }
+            };
+
+            Engine engine = new Engine();
+            decimal expectedCartTotal = 280;
 
             // Act
             engine.Run(cart);
