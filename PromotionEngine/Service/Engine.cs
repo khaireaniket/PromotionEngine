41d3046ce867b212f1829efd45bfed09996170e3
khaireaniket <khaireaniket@gmail.com>
2020-08-30 20:41:30 +0530

    Code refactored to fix unit test for scenario 1 of use case

--- PromotionEngine/Service/Engine.cs
+++ PromotionEngine/Service/Engine.cs
@@ -11,6 +11,7 @@ namespace PromotionEngine.Service
     {
         List<PromotionCollection> PromotionCollection;
         List<SkuCollection> SkuCollection;
+        Dictionary<(int, List<string>), decimal> PromotionAppliedWithPriceForSkus;
 
         public Engine()
         {
@@ -20,6 +21,9 @@ namespace PromotionEngine.Service
             // Get promotion and Sku collections from mock database
             PromotionCollection = MockDatabase.PromotionCollection;
             SkuCollection = MockDatabase.SkuCollection;
+
+            // Dictionary will be used to hold SkuIds which are applied a promotion with fixed price
+            PromotionAppliedWithPriceForSkus = new Dictionary<(int, List<string>), decimal>();
         }
 
         public Cart Run(Cart cart)
@@ -40,21 +44,38 @@ namespace PromotionEngine.Service
                                                                     .Any())
                                                         .ToList();
 
+            Random rnd = new Random();
 
             // Looping over all the filterd promotions
             foreach (var promotion in promotionsWithPurchasedSku)
             {
                 // If the iterated promotion has only one Sku item
-                if (promotion.PromotedSkus.Count == 1) 
+                if (promotion.PromotedSkus.Count == 1)
                 {
-                    // If purchased Sku item quantity is less than that of promotional sku item quantity
+                    var promotedSku = promotion.PromotedSkus.FirstOrDefault();
+                    var purchasedSku = cart.PurchasedSkus.FirstOrDefault(a => a.SkuId == promotedSku.SkuId);
+                    // Get correponding Sku entry from mock database collection
+                    var skuRow = SkuCollection.FirstOrDefault(a => a.SkuId == promotedSku.SkuId);
 
+                    // If purchased Sku item quantity is less than that of promotional sku item quantity apply base price of the Sku item
+                    if (purchasedSku.Quantity < promotedSku.Quantity)
+                    {
+                        PromotionAppliedWithPriceForSkus.Add((rnd.Next(-100, -1), new List<string> { purchasedSku.SkuId }), purchasedSku.Quantity * skuRow.Price);
+                    }
+                    // If purchased Sku item quantity is same as that of promotional sku item quantity directly apply offer
+                    else if (purchasedSku.Quantity == promotedSku.Quantity)
+                    {
+                        PromotionAppliedWithPriceForSkus.Add((promotion.PromotionId, new List<string> { purchasedSku.SkuId }), promotion.Offer);
+                    }
+                    // If purchased Sku item quantity is more than that of promotional sku item quantity calculate price with offer
+                    else if (purchasedSku.Quantity > promotedSku.Quantity)
+                    {
+                        var parts = purchasedSku.Quantity / promotedSku.Quantity;
+                        var reminder = purchasedSku.Quantity % promotedSku.Quantity;
 
-                    // If purchased Sku item quantity is same as that of promotional sku item quantity
-
-
-                    // If purchased Sku item quantity is more than that of promotional sku item quantity
-
+                        var price = (parts * promotion.Offer) + (reminder * skuRow.Price);
+                        PromotionAppliedWithPriceForSkus.Add((promotion.PromotionId, new List<string> { purchasedSku.SkuId }), price);
+                    }
                 }
                 // If the iterated promotion has more than one Sku item
                 else if (promotion.PromotedSkus.Count > 1) 
@@ -63,6 +84,38 @@ namespace PromotionEngine.Service
                 }
             }
 
+            // List for holding purchased Sku items which are not applicable for promotion
+            List<PurchasedSku> remainingSkuList = new List<PurchasedSku>();
+
+            List<string> promotionAppliedSkus = new List<string>();
+
+            // Dictionary key consisting of PromotionId and List of Sku items
+            var keys = PromotionAppliedWithPriceForSkus.Keys;
+            foreach (var key in keys)
+            {
+                // Get all the Sku items which are applied in a promotion
+                promotionAppliedSkus.AddRange(key.Item2);
+            }
+            // Distinct Sku items
+            promotionAppliedSkus = promotionAppliedSkus.Distinct().ToList();
+
+            // Get all the Sku items which are not part of any promotion
+            var nonPromotedSkuList = purchasedSkuIds.Except(promotionAppliedSkus).ToList();
+            foreach (var sku in nonPromotedSkuList)
+            {
+                var purchasedSku = cart.PurchasedSkus.FirstOrDefault(a => a.SkuId == sku);
+                // Add non promoted sku item in remaining sku item list
+                remainingSkuList.Add(new PurchasedSku { SkuId = purchasedSku.SkuId, Quantity = purchasedSku.Quantity });
+            }
+
+            foreach (var remainingSku in remainingSkuList)
+            {
+                var skuRow = SkuCollection.FirstOrDefault(a => a.SkuId == remainingSku.SkuId);
+                // Apply base price of the Sku item
+                PromotionAppliedWithPriceForSkus.Add((rnd.Next(-100, -1), new List<string> { remainingSku.SkuId }), remainingSku.Quantity * skuRow.Price);
+            }
+
+            cart.CartTotal = PromotionAppliedWithPriceForSkus.Sum(a => a.Value);
             return cart;
         }
     }
